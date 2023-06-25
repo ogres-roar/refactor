e64eceeccfb8d128639e26d0d6e11989ef7a8b6a
ogres <1771626730@qq.com>
2023-06-26 02:05:10 +0800

    construt unitest

--- after/car/driver_test.cpp
+++ after/car/driver_test.cpp
@@ -1,9 +1,52 @@
 #include "gtest/gtest.h"
+#include "gmock/gmock.h"
+#include "driver.h"
 
-TEST(HelloTest, BasicAssertions)
+using ::testing::ElementsAreArray;
+
+TEST(driver_test, nocar)
 {
-    // Expect two strings not to be equal.
-    EXPECT_STRNE("hello", "world");
-    // Expect equality.
-    EXPECT_EQ(7 * 6, 42);
+    Route r = std::vector<Move>{
+        {
+            .action = Action::start,
+        },
+        {
+            .action = Action::straight,
+            .distance = 10,
+        },
+        {
+            .action = Action::turn,
+            .direction = Direction::left,
+        },
+        {
+            .action = Action::straight,
+            .distance = 20,
+        },
+        {
+            .action = Action::turn,
+            .direction = Direction::right,
+        },
+        {
+            .action = Action::stop,
+        }};
+    Driver li("li");
+    auto rt = li.drive(r);
+    EXPECT_THAT(rt, ElementsAreArray({"driver don't kwon how to start!",
+                                      "driver don't kwon how to go straight!",
+                                      "driver don't kwon how to turn right!",
+                                      "driver don't kwon how to go straight!",
+                                      "driver don't kwon how to turn left!",
+                                      "driver don't kwon how to stop!"}));
+}
+
+TEST(driver_test, bmw)
+{
+}
+
+TEST(driver_test, benz)
+{
+}
+
+TEST(driver_test, audi)
+{
 }
